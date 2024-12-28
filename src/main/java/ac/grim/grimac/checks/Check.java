4c41e6e753e061dd29158110780dc6cd8ff15817
Jones <admin@jonesdev.xyz>
2024-12-29 00:32:24 +0100

    Increase violations before anything else

--- src/main/java/ac/grim/grimac/checks/Check.java
+++ src/main/java/ac/grim/grimac/checks/Check.java
@@ -88,14 +88,12 @@ public class Check extends GrimProcessor implements AbstractCheck {
         if (player.disableGrim || (experimental && !player.isExperimentalChecks()) || exempted)
             return false; // Avoid calling event if disabled
 
+        violations++;
         FlagEvent event = new FlagEvent(player, this);
         Bukkit.getPluginManager().callEvent(event);
         if (event.isCancelled()) return false;
 
-
         player.punishmentManager.handleViolation(this);
-
-        violations++;
         return true;
     }
 
