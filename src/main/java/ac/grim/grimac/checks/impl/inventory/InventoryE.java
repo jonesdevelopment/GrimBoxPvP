57cde6f35c63d1a13e121a0d2882e443d1a95a16
Jones <admin@jonesdev.xyz>
2025-01-29 15:01:00 +0100

    Fix InventoryE false positive

--- src/main/java/ac/grim/grimac/checks/impl/inventory/InventoryE.java
+++ src/main/java/ac/grim/grimac/checks/impl/inventory/InventoryE.java
@@ -2,24 +2,42 @@ package ac.grim.grimac.checks.impl.inventory;
 
 import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
-import ac.grim.grimac.checks.type.RotationCheck;
+import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
-import ac.grim.grimac.utils.anticheat.update.RotationUpdate;
+import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientClientStatus;
 
 @CheckData(name = "InventoryE")
-public class InventoryE extends Check implements RotationCheck {
+public class InventoryE extends Check implements PacketCheck {
     public InventoryE(final GrimPlayer player) {
         super(player);
     }
 
+    private boolean exempt;
+
     @Override
-    public void process(final RotationUpdate rotationUpdate) {
-        if (player.hasInventoryOpen
-                && !player.skippedTickInActualMovement
-                && !player.packetStateData.lastPacketWasTeleport
-                && !player.packetStateData.lastPacketWasOnePointSeventeenDuplicate
-                && flagAndAlert()) {
-            player.closeInventory();
+    public void onPacketReceive(final PacketReceiveEvent event) {
+        if (event.getPacketType().equals(PacketType.Play.Client.PLAYER_ROTATION)
+                || event.getPacketType().equals(PacketType.Play.Server.PLAYER_POSITION_AND_LOOK)) {
+            if (!player.hasInventoryOpen) {
+                return;
+            }
+            if (exempt) {
+                exempt = false;
+                return;
+            }
+            if (!player.skippedTickInActualMovement
+                    && !player.packetStateData.lastPacketWasTeleport
+                    && !player.packetStateData.lastPacketWasOnePointSeventeenDuplicate
+                    && flagAndAlert()) {
+                player.closeInventory();
+            }
+        } else if (event.getPacketType() == PacketType.Play.Client.CLIENT_STATUS) {
+            WrapperPlayClientClientStatus wrapper = new WrapperPlayClientClientStatus(event);
+            if (wrapper.getAction() == WrapperPlayClientClientStatus.Action.OPEN_INVENTORY_ACHIEVEMENT) {
+                exempt = true;
+            }
         }
     }
 }
